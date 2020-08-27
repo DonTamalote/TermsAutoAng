23cf1f93a16fda0a25550b33f7237a9662796e88
Aldo Rivera <aldo.rivera@outlook.com>
2020-08-27 11:46:27 -0600

    Hola

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Generador eCommerce';
+  empresa = '';
+  titular = '';
+  sitio = '';
+  correov = '';
+  direccion = '';
+  municipio = '';
+  estado = '';
+  cp = '';
+  pais = '';
+}
